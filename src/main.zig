73614db4ee9ee9e78211a6eba4d0c3231f54d87b
pandadiestro <rojoalsa2@gmail.com>
2025-04-08 14:57:25 -0500

    feat: finished adding the lexer
    TODO:
    - lexical analysis + parser

--- src/main.zig
+++ src/main.zig
@@ -1,66 +1,20 @@
 const std = @import("std");
-const log = std.log.scoped(.@"examples/basic");
+const thwomp = @import("lib/stomp/lib.zig");
 
-const zzz = @import("zzz");
-const http = zzz.HTTP;
+pub fn main() !void {
+    const test_str =
+        \\SEND
+        \\is
+        \\ some text
+        \\ EOL
+        \\
+    ;
 
-const tardy = zzz.tardy;
-const Tardy = tardy.Tardy(.auto);
-const Runtime = tardy.Runtime;
-const Socket = tardy.Socket;
-
-const Server = http.Server;
-const Router = http.Router;
-const Context = http.Context;
-const Route = http.Route;
-const Respond = http.Respond;
-
-fn base_handler(ctx: *const Context, _: void) !Respond {
-    return ctx.response.apply(.{
-        .status = .OK,
-        .mime = http.Mime.HTML,
-        .body = "Hello, world!",
+    var buffer: [4096]u8 = @splat(0);
+    var fba = std.heap.FixedBufferAllocator.init(buffer[0..]);
+    const lexemes = try thwomp.Lexer.tokenizeAll(test_str, fba.allocator());
+    std.debug.print("lexemes:\n{any}\n", .{
+        lexemes,
     });
 }
 
-pub fn main() !void {
-    const host: []const u8 = "0.0.0.0";
-    const port: u16 = 9862;
-
-    var gpa = std.heap.GeneralPurposeAllocator(.{ .thread_safe = true }){};
-    const allocator = gpa.allocator();
-    defer _ = gpa.deinit();
-
-    var t = try Tardy.init(allocator, .{ .threading = .auto });
-    defer t.deinit();
-
-    var router = try Router.init(allocator, &.{
-        Route.init("/").get({}, base_handler).layer(),
-    }, .{});
-    defer router.deinit(allocator);
-
-    var socket = try Socket.init(.{ .tcp = .{ .host = host, .port = port } });
-    defer socket.close_blocking();
-    try socket.bind();
-    try socket.listen(4096);
-
-    const EntryParams = struct {
-        router: *const Router,
-        socket: Socket,
-    };
-
-    try t.entry(
-        EntryParams{ .router = &router, .socket = socket },
-        struct {
-            fn entry(rt: *Runtime, p: EntryParams) !void {
-                var server = Server.init(.{
-                    .stack_size = 1024 * 1024 * 4,
-                    .socket_buffer_bytes = 1024 * 2,
-                    .keepalive_count_max = null,
-                    .connection_count_max = 1024,
-                });
-                try server.serve(rt, p.router, .{ .normal = p.socket });
-            }
-        }.entry,
-    );
-}
