14b068db691702ba10c4b2fee3ef205fde9c1620
pandadiestro <rojoalsa2@gmail.com>
2025-06-27 14:08:35 -0500

    fix: test new flake

--- flake.nix
+++ flake.nix
@@ -10,26 +10,41 @@
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
     in {
-      packages.x86_64-linux.default = pkgs.stdenv.mkDerivation {
-        nativeBuildInputs = [
-          pkgs.binutils
-          pkgs.zig
-        ];
+      packages.x86_64-linux.default =
+        let
+          deps = pkgs.stdenv.mkDerivation {
+            pname = "thwomp-deps";
+            version = "0.0.0";
+            src = ./.;
 
-        pname = "thwomp";
-        version = "0.1.0";
-        src = ./.;
+            nativeBuildInputs = [
+              pkgs.zig
+              pkgs.binutils
+            ];
 
-        buildPhase = ''
-            export ZIG_GLOBAL_CACHE_DIR=$TMPDIR/zig-cache
-            zig build
+            buildPhase = ''
+            zig build -Doptimize=ReleaseSafe --global-cache-dir $TMPDIR/zig-cache
           '';
 
-        installPhase = ''
+            installPhase = ''
             mkdir -p $out/bin
             cp zig-out/bin/* $out/bin/
           '';
-      };
+
+            outputHashAlgo = "sha256";
+            outputHashMode = "recursive";
+            outputHash = "";
+          };
+        in
+          pkgs.stdenv.mkDerivation {
+            pname = "thwomp";
+            version = "1.0.0";
+            src = ./.;
+
+            nativeBuildInputs = [
+              deps
+            ];
+          };
 
       devShells.x86_64-linux.default = pkgs.mkShell {
         buildInputs = [
@@ -39,5 +54,5 @@
           pkgs.postgresql
         ];
       };
-  };
+    };
 }
