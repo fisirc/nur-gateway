aff9f94c8e8a364fd9ddf665f6384ac15f3afe30
pandadiestro <rojoalsa2@gmail.com>
2025-06-27 23:28:53 -0500

    fix: test new flake

--- flake.nix
+++ flake.nix
@@ -23,17 +23,21 @@
             ];
 
             buildPhase = ''
-            zig build -Doptimize=ReleaseSafe --global-cache-dir $TMPDIR/zig-cache
+            mkdir $TMPDIR/zig-cache
+            export ZIG_GLOBAL_CACHE_DIR=$TMPDIR/zig-cache
+            zig build --fetch
           '';
 
             installPhase = ''
-            mkdir -p $out/bin
-            cp zig-out/bin/* $out/bin/
+            mkdir -p $out/cache
+            cp -r $TMPDIR/zig-cache $out/cache
           '';
+
+            dontPatchShebangs = true;
 
             outputHashAlgo = "sha256";
             outputHashMode = "recursive";
-            outputHash = "";
+            outputHash = "sha256-G8zEUjtypOyS3DdpXXHJDW2r469CDGcHgcf0aDFPMwg=";
           };
         in
           pkgs.stdenv.mkDerivation {
@@ -42,8 +46,19 @@
             src = ./.;
 
             nativeBuildInputs = [
-              deps
+              pkgs.zig
+              pkgs.binutils
             ];
+
+            buildPhase = ''
+           export ZIG_GLOBAL_CACHE_DIR=${deps}/cache
+           zig build -Doptimize=ReleaseSafe
+           '';
+
+            installPhase = ''
+            mkdir -p $out/bin
+            cp zig-out/bin/* $out/bin/
+            '';
           };
 
       devShells.x86_64-linux.default = pkgs.mkShell {
